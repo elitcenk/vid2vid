52d8103544525960c2061168e74c7ca8375f82a6
elitcenk <calp@netas.com.tr>
2021-01-10 20:17:24 +0300

    download file fixed.

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-### Copyright (C) 2017 NVIDIA Corporation. All rights reserved. 
+### Copyright (C) 2017 NVIDIA Corporation. All rights reserved.
 ### Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 import time
 import os
@@ -13,7 +13,7 @@ from util.visualizer import Visualizer
 from util import html
 
 opt = TestOptions().parse(save=False)
-opt.nThreads = 1   # test code only supports nThreads = 1
+opt.nThreads = 1  # test code only supports nThreads = 1
 opt.batchSize = 1  # test code only supports batchSize = 1
 opt.serial_batches = True  # no shuffle
 opt.no_flip = True  # no flip
@@ -30,7 +30,7 @@ save_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.whi
 print('Doing %d frames' % len(dataset))
 for i, data in enumerate(dataset):
     if i >= opt.how_many:
-        break    
+        break
     if data['change_seq']:
         model.fake_B_prev = None
 
@@ -39,16 +39,16 @@ for i, data in enumerate(dataset):
     B = Variable(data['B']).view(1, -1, opt.output_nc, height, width) if len(data['B'].size()) > 2 else None
     inst = Variable(data['inst']).view(1, -1, 1, height, width) if len(data['inst'].size()) > 2 else None
     generated = model.inference(A, B, inst)
-    
+
     if opt.label_nc != 0:
         real_A = util.tensor2label(generated[1], opt.label_nc)
     else:
         c = 3 if opt.input_nc == 3 else 1
-        real_A = util.tensor2im(generated[1][:c], normalize=False)    
-        
-    visual_list = [('real_A', real_A), 
+        real_A = util.tensor2im(generated[1][:c], normalize=False)
+
+    visual_list = [('real_A', real_A),
                    ('fake_B', util.tensor2im(generated[0].data[0]))]
-    visuals = OrderedDict(visual_list) 
+    visuals = OrderedDict(visual_list)
     img_path = data['A_path']
     print('process image... %s' % img_path)
     visualizer.save_images(save_dir, visuals, img_path)
